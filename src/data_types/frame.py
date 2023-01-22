b95611126eb976e52110267915eb23501a12d996
Pablo <pablete.arino@gmail.com>
2023-01-22 18:45:07 +0100

    Add documentation to Frame class

--- src/data_types/frame.py
+++ src/data_types/frame.py
@@ -1,19 +1,26 @@
-from typing import List, Tuple
+from typing import List
 
 import cv2
 from dataclasses import dataclass, field
-from collections import namedtuple
 import numpy as np
 
-
-Rectangle = namedtuple("Rectangle", ["x", "y", "w", "h"])
-LabeledRectangle = namedtuple("LabeledRectangle", ["label", "x", "y", "w", "h"])
-Point = Tuple[int, int]
-Color = Tuple[int, int, int]
+from .types import Point, Color, Rectangle
 
 
 @dataclass
 class Frame:
+    """A frame of a video.
+
+    Args:
+        image: the image of the frame
+
+    Examples:
+        >>> frame = Frame(np.zeros((100, 100, 3), np.uint8))
+        >>> frame.height
+        100
+        >>> frame.width
+        100
+    """
 
     image: np.ndarray
     width: int = field(init=False)
@@ -23,9 +30,16 @@ class Frame:
         self.height, self.width, _ = self.image.shape
 
     def draw_rectangles(self,
-                        rectangles: List[LabeledRectangle],
+                        rectangles: List[Rectangle],
                         color: Color = (0, 255, 0),
                         thickness: int = 2):
+        """Draws the rectangles on the frame.
+
+        Args:
+            rectangles: the rectangles to draw
+            color: the color of the rectangles. Defaults to (0, 255, 0) (green).
+            thickness: the thickness of the rectangles. Defaults to 2.
+        """
         for rectangle in rectangles:
             _, x, y, w, h = rectangle
             cv2.rectangle(self.image, (x, y), (x + w, y + h), color, thickness)
@@ -34,7 +48,12 @@ class Frame:
                        rectangle: Rectangle,
                        color: Color = (0, 255, 0),
                        thickness: int = 2):
-        """Draws the rectangle on the frame
+        """Draws the rectangle on the frame.
+
+        Args:
+            rectangle: the rectangle to draw
+            color: the color of the rectangle. Defaults to (0, 255, 0) (green).
+            thickness: the thickness of the rectangle. Defaults to 2.
         """
         x, y, w, h = rectangle
         cv2.rectangle(self.image, (x, y), (x + w, y + h), color, thickness)
@@ -44,6 +63,14 @@ class Frame:
                   position: Point,
                   color: Color = (0, 255, 0),
                   thickness: int = 2):
+        """Draws the text on the frame.
+
+        Args:
+            text: the text to draw
+            position: the position of the text
+            color: the color of the text. Defaults to (0, 255, 0) (green).
+            thickness: the thickness of the text. Defaults to 2.
+        """
         cv2.putText(self.image, text, position, cv2.FONT_HERSHEY_SIMPLEX, 1, color, thickness)
 
     def draw_line(self,
@@ -51,4 +78,12 @@ class Frame:
                   end: Point,
                   color: Color = (0, 255, 0),
                   thickness: int = 2):
+        """Draws the line on the frame.
+
+        Args:
+            start: the start of the line
+            end: the end of the line
+            color: the color of the line. Defaults to (0, 255, 0) (green).
+            thickness: the thickness of the line. Defaults to 2.
+        """
         cv2.line(self.image, start, end, color, thickness)
