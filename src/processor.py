a9f4ce4dccf7e3f71209e770113c130fb9e6e5f7
Pablo <pablete.arino@gmail.com>
2023-01-22 21:32:12 +0100

    Documentation

--- src/processor.py
+++ src/processor.py
@@ -11,12 +11,14 @@ class Processor:
     Args:
         car_detector: the car detector to use
         action_zone: the action zone to use. If not specified, the whole frame is used. If specified, it must be a
-            tuple of 2 to 4 integers, where the first two integers are the top left corner of the action zone, and the
-            last two integers are the width and height of the action zone. If only 2 integers are specified, the action
-            zone is a square with the frame's width and height at the left and behind the given point.
+            tuple of 4 integers, where the first two integers are the top left corner of the action zone, and the
+            last two integers are the width and height of the action zone. These two last integers can be set to None
+            to use the whole width or height of the frame until the right or bottom border of the frame.
     """
 
-    def __init__(self, car_detector: CarDetector, action_zone: Optional[Tuple[int, ...]] = None):
+    def __init__(self,
+                 car_detector: CarDetector,
+                 action_zone: Optional[Tuple[int, int, Optional[int], Optional[int]]] = None):
         self.car_detector = car_detector
         self.action_zone = action_zone
 
@@ -30,11 +32,10 @@ class Processor:
         for frame in video:
             if self.action_zone is None:
                 self.action_zone = Rectangle(0, 0, frame.width, frame.height - 125)
-            if isinstance(self.action_zone, tuple) and len(self.action_zone) == 2:
-                x, y = self.action_zone
-                self.action_zone = Rectangle(x, y, frame.width - x, frame.height - y)
-            if isinstance(self.action_zone, tuple) and len(self.action_zone) == 4:
+            if isinstance(self.action_zone, tuple):
                 x, y, w, h = self.action_zone
+                w = w if w is not None else frame.width - x
+                h = h if h is not None else frame.height - y
                 self.action_zone = Rectangle(x, y, w, h)
 
             cars_rectangles = self.car_detector.detect(frame.image)
