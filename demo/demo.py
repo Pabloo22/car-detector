0a99063fbd44a93626558328ffa73c69a1093f5f
Pablo <pablete.arino@gmail.com>
2023-01-28 13:39:05 +0100

    Update demo

--- demo/demo.py
+++ demo/demo.py
@@ -22,8 +22,8 @@ def detector_clasico():
     """
     __MÉTODO CLÁSICO__
     Para el método  clásico  hemos hecho  lo siguiente. Primero,  hemos tomado dos frames del video, los pasamos a escala de
-    grises y realizamos la diferencia entre estos fotogramas.De esta forma capturamos el movimiento de un frame a otro.
-    Luego, el resultado de esa diferencia  apreciamos los  coches en blanco, por lo que con un filtro de unos de dimensiones
+    grises y realizamos la diferencia entre estos fotogramas. De esta forma capturamos el movimiento de un frame a otro.
+    Luego, con el resultado de esa diferencia  apreciamos los  coches en blanco, por lo que con un filtro de unos de dimensiones
     5x5 realizamos una dilatación a la imagen para obtener estructuras cuadradas en vez de coches, y  capturar de esta forma
     todos los pixeles separados y que queden unidos. A continuación, aplicamos un blur a la imagen para eliminar los pixeles
     diminutos que existan en la imagen y suavizar los bordes. Por último, detectamos  el contorno de las 'manchas blancas' y
@@ -50,11 +50,13 @@ def detector_yolo():
     __MÉTODO REDES NEURONALES__
     Para el método con redes neuronales hemos aplicado el algoritmo de YOLOv5 para detección de coches. Hemos cargado
     directamente un repositorio de github donde está el modelo entrenado y lo hemos aplicado en el programa. Como se podrá
-    ver, el algoritmo es relativamente más lento en ejecutar (3-10 min), por lo que lo dejamos comentado por este motivo. En caso de
-    querer comprobar su funcionamiento, descomente el código.
+    ver, el algoritmo es relativamente más lento en ejecutar (3-10 min) en CPU, por lo que lo dejamos comentado por este motivo.
+    En caso de querer comprobar su funcionamiento, descomente el código.
     """
     # Importamos el detector YOLOv5
     car_detector = YoloDetector()
+
+    # Creamos un objeto Tracker para modificar los parametros por defecto
     tracker = Tracker(min_trace_length=12)
     
     # Creamos un objeto Processor
